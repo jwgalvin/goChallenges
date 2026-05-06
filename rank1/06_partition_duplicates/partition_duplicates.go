2b89d842424b479f1d17f8c13359755dbdb2c4a8
Joe Galvin <jwgalvin@gmail.com>
2026-05-06 12:06:05 -0600

    Solution with 0n time

--- rank1/06_partition_duplicates/partition_duplicates.go
+++ rank1/06_partition_duplicates/partition_duplicates.go
@@ -4,6 +4,18 @@ package partition_duplicates
 //   - duplicates: each value that appears more than once, mapped to its count
 //   - uniques: each value that appears exactly once, mapped to its count (always 1)
 func PartitionDuplicates(nums []int) (duplicates map[int]int, uniques map[int]int) {
-	// TODO: implement
-	return map[int]int{}, map[int]int{}
+	duplicates = make(map[int]int)
+	uniques = make(map[int]int)
+
+	for _, num := range nums {
+		if _, ok := duplicates[num]; ok {
+			duplicates[num]++
+		} else if _, ok := uniques[num]; ok {
+			duplicates[num] = 2
+			delete(uniques, num)
+		} else {
+			uniques[num] = 1
+		}
+	}
+	return
 }
