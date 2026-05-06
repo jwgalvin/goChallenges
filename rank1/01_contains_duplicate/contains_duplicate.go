637a25720163a51ecc0c8ae5778aa9debbe8d388
Joe Galvin <jwgalvin@gmail.com>
2026-05-06 11:43:49 -0600

    First solution

--- rank1/01_contains_duplicate/contains_duplicate.go
+++ rank1/01_contains_duplicate/contains_duplicate.go
@@ -1,7 +1,20 @@
 package contains_duplicate
 
+
 // ContainsDuplicate returns true if any value in nums appears more than once.
 func ContainsDuplicate(nums []int) bool {
-	// TODO: implement
+	seen := []int{}
+
+	for _, num := range nums {
+		if seen == nil {
+			seen = append(seen, num)
+		}
+		for _, n := range seen {
+			if n == num {
+				return true
+			}
+		}
+		seen = append(seen, num)
+	}
 	return false
 }
